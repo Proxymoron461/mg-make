bc7b1c292bc9f30c5e145873f381dd868389eeec
T <tobyjonahbee@gmail.com>
2020-04-17 15:02:44 +0100

    Runs in background at end, better formatting.

--- make-mg.sh
+++ make-mg.sh
@@ -7,7 +7,7 @@ if [[ $1 == "-e" ]]; then
     shift
 fi
 
-if ls | grep -q "$1" ; then
+if ls | grep -q "$1$" ; then
     echo "A folder/file with this name already exists in this directory!"
     echo "Please try again with a different name."
     exit 1
@@ -27,7 +27,12 @@ dotnet new mgdesktopgl
 dotnet new sln
 
 if [[ $extended == "true" ]] ; then
+    printf "\n\n"
     echo "Adding package MonoGame.Extended..."
     dotnet add package MonoGame.Extended
 fi
 
+printf "\n\n"
+echo "Running the blank project in the background to check it works. Press the ESC key to exit."
+
+dotnet run &
